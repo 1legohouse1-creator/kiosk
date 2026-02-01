14e2bb7ecb959450826919a57b91f35b9b83caad
1legohouse1-creator <1legohouse1@gmail.com>
2026-02-01 14:57:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ def menu_button(category_name) :
     for name, price in current_menu.items() :
         st.write(name)
         st.write(f"{price} 원")
-        st.button("담기")
+        if st.button("담기", key=name) :
+            st.write(f"{name} 장바구니 담기")
     
 
 with tab1 : menu_button("식사류")
